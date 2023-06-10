c089774eef18b9dbb7144701a42e9a4f85470ee0
робот <vaginalnaymamba@mail.ru>
2023-06-11 00:10:35 +0300

    start hm

---
+++ src/test/java/pro/sky/java/course2/demo1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
